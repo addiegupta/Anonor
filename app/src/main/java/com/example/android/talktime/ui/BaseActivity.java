90a214daa1c8a00035f5a75a18b5773ba504bae8
Aditya <addiegupta@gmail.com>
2017-09-28 07:49:04 +0530

    Setup basic Firebase sign in and signup

---
+++ app/src/main/java/com/example/android/talktime/ui/BaseActivity.java
@@ -0,0 +1,50 @@
+package com.example.android.talktime.ui;
+
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.android.talktime.CallService;
+
+public class BaseActivity extends AppCompatActivity implements ServiceConnection{
+
+    private CallService.CallServiceInterface mCallServiceInterface;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getApplicationContext().bindService(new Intent(this, CallService.class), this,
+                BIND_AUTO_CREATE);
+    }
+
+    @Override
+    public void onServiceConnected(ComponentName name, IBinder iBinder) {
+        if (CallService.class.getName().equals(name.getClassName())) {
+            mCallServiceInterface = (CallService.CallServiceInterface) iBinder;
+            onServiceConnected();
+        }
+    }
+
+    @Override
+    public void onServiceDisconnected(ComponentName componentName) {
+        if (CallService.class.getName().equals(componentName.getClassName())) {
+            mCallServiceInterface = null;
+            onServiceDisconnected();
+        }
+    }
+
+    protected void onServiceConnected() {
+        // for subclasses
+    }
+
+    protected void onServiceDisconnected() {
+        // for subclasses
+    }
+
+    protected CallService.CallServiceInterface getSinchServiceInterface() {
+        return mCallServiceInterface;
+    }
+}
---
+++ app/src/main/java/com/example/android/talktime/ui/PlaceCallActivity.java
@@ -0,0 +1,101 @@
+package com.example.android.talktime.ui;
+
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.android.talktime.CallService;
+import com.example.android.talktime.R;
+import com.sinch.android.rtc.MissingPermissionException;
+import com.sinch.android.rtc.calling.Call;
+
+public class PlaceCallActivity extends BaseActivity {
+
+
+    private Button mCallButton;
+    private EditText mCallName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_place_call);
+
+        mCallName = (EditText) findViewById(R.id.callName);
+        mCallButton = (Button) findViewById(R.id.callButton);
+        mCallButton.setEnabled(false);
+        mCallButton.setOnClickListener(buttonClickListener);
+
+        Button stopButton = (Button) findViewById(R.id.stopButton);
+        stopButton.setOnClickListener(buttonClickListener);
+    }
+
+    @Override
+    protected void onServiceConnected() {
+        TextView userName = (TextView) findViewById(R.id.loggedInName);
+        userName.setText(getSinchServiceInterface().getUserName());
+        mCallButton.setEnabled(true);
+    }
+
+    private void stopButtonClicked() {
+        if (getSinchServiceInterface() != null) {
+            getSinchServiceInterface().stopClient();
+        }
+        finish();
+    }
+
+    private void callButtonClicked() {
+        String userName = mCallName.getText().toString();
+        if (userName.isEmpty()) {
+            Toast.makeText(this, "Please enter a user to call", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        try {
+            Call call = getSinchServiceInterface().callUser(userName);
+            if (call == null) {
+                // Service failed for some reason, show a Toast and abort
+                Toast.makeText(this, "Service is not started. Try stopping the service and starting it again before "
+                        + "placing a call.", Toast.LENGTH_LONG).show();
+                return;
+            }
+            String callId = call.getCallId();
+            Intent callScreen = new Intent(this, CallScreenActivity.class);
+            callScreen.putExtra(CallService.CALL_ID, callId);
+            startActivity(callScreen);
+        } catch (MissingPermissionException e) {
+            ActivityCompat.requestPermissions(this, new String[]{e.getRequiredPermission()}, 0);
+        }
+
+    }
+
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(this, "You may now place a call", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(this, "This application needs permission to use your microphone to function properly.", Toast
+                    .LENGTH_LONG).show();
+        }
+    }
+
+    private View.OnClickListener buttonClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.callButton:
+                    callButtonClicked();
+                    break;
+
+                case R.id.stopButton:
+                    stopButtonClicked();
+                    break;
+
+            }
+        }
+    };
+}
